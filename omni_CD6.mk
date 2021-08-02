af768dd232b1999b941181acc5f10b01ddb1b23b
mastersenpai05 <pampangavergil0405@gmail.com>
2021-08-03 01:42:03 +0800

    CD6: Define need mod and lib

--- omni_CD6.mk
+++ omni_CD6.mk
@@ -35,6 +35,18 @@ PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO CD6
 PRODUCT_MANUFACTURER := TECNO MOBILE LIMITED
 
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice \
+    ashmemd_aidl_interface-cpp \
+    libashmemd_client
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so
+
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31
