5893a97dbe61ca2bf780c541de69cfb274c98745
Mikesew1320 <mikesew1320@gmail.com>
2021-08-10 07:52:23 +0300

    Ofox Vendor

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+export TARGET_ARCH=arm64
+export OF_STATUS_INDENT_LEFT=48
+export OF_OTA_RES_DECRYPT=1
+export OF_STATUS_INDENT_RIGHT=48
+export OF_HIDE_NOTCH=1
+export OF_CLOCK_POS=0
+export OF_MAINTAINER=Mikesew1320
+export OF_FL_PATH1="/sys/devices/virtual/torch/torch/torch_level"
