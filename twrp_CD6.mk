f19c13ff26e56efa32888068cdc0e6e755d6e71e
Michael Bekele <mikesew1320@gmail.com>
2026-03-23 23:17:38 +0300

    Update twrp_CD6.mk
    Mar 23 below

--- twrp_CD6.mk
+++ twrp_CD6.mk
@@ -57,3 +57,39 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31
+
+#Mar 23, 2026
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/tecno/CD6
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
+
+PRODUCT_PACKAGES += \
+    bootctrl.mt6762
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# Boot Control HAL for recovery (shared/recovery variants)
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service \
+    bootctrl.$(TARGET_BOOTLOADER_BOARD_NAME).recovery
